55f84f45dc1ff4535e61d31675bea17dddda8bbf
rocosen <49807007+rocosen@users.noreply.github.com>
2023-04-23 15:52:43 +0800

    Update App.tsx

--- app/src/views/App/App.tsx
+++ app/src/views/App/App.tsx
@@ -33,9 +33,24 @@ class App extends React.Component<RoutedProps> {
 
   protected loadingReaction?: IReactionDisposer;
 
+  GetQueryString = (name: any) => {
+    var reg: any = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r: any = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+    var context = '';
+    if (r != null) context = decodeURIComponent(r[2]);
+    reg = null;
+    r = null;
+    return context == null || context == '' || context == 'undefined' ? '' : context;
+  };
+
   componentDidMount() {
     console.log('App->componentDidMount');
     const { store, connection } = this.props;
+
+    if(this.GetQueryString('code')){
+      console.log('th', this.GetQueryString('code'));
+      localStorage.setItem('code', this.GetQueryString('code'));
+    }
 
     this.loadingReaction = reaction(
       () => store.uiStore.loading,
